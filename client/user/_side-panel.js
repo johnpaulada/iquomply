5f4fbe18af53fa5d10d47ade8be24e82776800b8
John Paul Ada <johnpaulada@Ferdinand-Mini.local.gap>
2016-08-02 13:17:40 +0800

    Temporarily removed sidebar

--- client/user/_side-panel.js
+++ client/user/_side-panel.js
@@ -1,6 +1,6 @@
 Template.userDashboardAside.onRendered(function() {
     $('.side-panel').scotchPanel({
-        containerSelector: 'body', // As a jQuery Selector
+        containerSelector: '#user-dashboard-aside', // As a jQuery Selector
         direction: 'left', // Make it toggle in from the left
         duration: 600, // Speed in ms how fast you want it to be
         transition: 'ease', // CSS3 transition type: linear, ease, ease-in, ease-out, ease-in-out, cubic-bezier(P1x,P1y,P2x,P2y)
