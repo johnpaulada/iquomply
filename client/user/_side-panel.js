dcd5463cf6ed42b6a5027211421bd25a992133bd
John Paul Ada <johnpaulada@Ferdinand-Mini.local.gap>
2016-08-02 12:01:49 +0800

    Created side bar

--- client/user/_side-panel.js
+++ client/user/_side-panel.js
@@ -5,7 +5,7 @@ Template.userDashboardAside.onRendered(function() {
         duration: 600, // Speed in ms how fast you want it to be
         transition: 'ease', // CSS3 transition type: linear, ease, ease-in, ease-out, ease-in-out, cubic-bezier(P1x,P1y,P2x,P2y)
         clickSelector: '.toggle-panel', // Enables toggling when clicking elements of this class
-        distanceX: '30%', // Size fo the toggle
+        distanceX: '300px', // Size fo the toggle
         enableEscapeKey: true // Clicking Esc will close the panel
     });
 });
