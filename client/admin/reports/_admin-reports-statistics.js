e9417b1b7ed48e7d589e791f55bd9c89d53d3965
Kristhiane Erol Yu <Kristhiane Erol Yu>
2016-09-13 00:02:25 +0800

    Added User Statistics Table

--- client/admin/reports/_admin-reports-statistics.js
+++ client/admin/reports/_admin-reports-statistics.js
@@ -3,14 +3,19 @@ Template.adminReportsStatistics.onCreated(function() {
 
     instance.unitShouldLoad = new ReactiveVar();
     instance.unitChartData  = new ReactiveVar();
+    instance.userChartData  = new ReactiveVar();
 
     instance.unitShouldLoad.set(false);
     instance.unitChartData.set(UnitChartDataGenerator.generate());
+    instance.userChartData.set(UserChartDataGenerator.generate());
 });
 
 Template.adminReportsStatistics.helpers({
     unitComplianceChartData: function() {
         return Template.instance().unitChartData.get();
+    },
+    userComplianceChartData: function() {
+        return Template.instance().userChartData.get();
     },
     unitShouldLoad() {
         return Template.instance().unitShouldLoad.get();
@@ -18,14 +23,29 @@ Template.adminReportsStatistics.helpers({
 });
 
 Template.adminReportsStatistics.onRendered(function() {
-    var details       = this.data.details;
-    var answers       = AnswerCounter.count(details.progress);
-    var unitChartData = Template.instance().unitChartData.get();
+    {
+        let details       = this.data.details;
+        let answers       = AnswerCounter.count(details.progress);
+        let unitChartData = Template.instance().unitChartData.get();
 
-    unitChartData.datasets[0].data[0] = answers.yes;
-    unitChartData.datasets[0].data[1] = answers.no;
-    unitChartData.datasets[0].data[2] = answers.partially;
+        unitChartData.datasets[0].data[0] = answers.yes;
+        unitChartData.datasets[0].data[1] = answers.no;
+        unitChartData.datasets[0].data[2] = answers.partially;
 
-    Template.instance().unitChartData.set(unitChartData);
+        Template.instance().unitChartData.set(unitChartData);
+    }
+
+    {
+        let details       = this.data.details;
+        let answers       = AnswerCounter.count(details.progress);
+        let userChartData = Template.instance().userChartData.get();
+
+        userChartData.datasets[0].data[0] = answers.yes;
+        userChartData.datasets[0].data[1] = answers.no;
+        userChartData.datasets[0].data[2] = answers.partially;
+
+        Template.instance().userChartData.set(userChartData);
+    }
+
     Template.instance().unitShouldLoad.set(true);
 });
