fc718c4d843979fef567c03ab0751d54f3d44b89
John Paul Ada <johnpaulada@gmail.com>
2016-09-20 04:52:41 -0700

    Added UI for individual analytics

--- client/admin/reports/_admin-reports-analytics.js
+++ client/admin/reports/_admin-reports-analytics.js
@@ -10,5 +10,24 @@ Template.adminReportsAnalytics.onCreated(function() {
 Template.adminReportsAnalytics.helpers({
     units() {
         return Template.instance().units();
+    },
+
+    hospitalWide() {
+        return hospitalWide();
     }
 });
+
+Template.adminReportsAnalytics.events({
+    'click #show-analytics-btn': function(event, instance) {
+        let unit = $('#unit-selection-input').val();
+        swal(`${unit} Analytics`, getAnalytics(unit));
+    }
+});
+
+function getHospitalWide() {
+    return '';
+}
+
+function getAnalytics(unit) {
+    return unit;
+}
