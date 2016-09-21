206b573db1b9a4d9273d7728a109f1fcfb7f2f4a
John Paul Ada <johnpaulada@Ferdinand-Mini.local.gap>
2016-09-21 11:17:26 +0800

    Individual Analytics

--- client/admin/reports/_admin-reports-analytics.js
+++ client/admin/reports/_admin-reports-analytics.js
@@ -31,33 +31,29 @@ function getHospitalWide() {
 function getAnalytics(unit) {
     import ActionExtractor from '../../lib/ActionExtractor';
 
-    const user = Meteor.users.findOne({'profile.unit': unit});
-    const name = user.profile.first_name + ' ' + user.profile.last_name;
-    const report = Reports.findOne({'userId': user._id}, {sort: {'dateSubmitted': -1}});
-    const answers = AnswerCounter.count(report.progress);
+    const user            = Meteor.users.findOne({'profile.unit': unit});
+    const name            = user.profile.firstName + ' ' + user.profile.lastName;
+    const report          = Reports.findOne({'userId': user._id}, {sort: {'dateSubmitted': -1}});
+    const answers         = AnswerCounter.count(report.progress);
     const complianceRatio = answers.yes / answers.total;
 
     if (complianceRatio > 0.9) {
         return `${unit} is performing well, with a compliance ratio of ${complianceRatio * 100}%`;
     } else {
-        const statusText = `${unit} is not performing well, with a compliance ratio of ${complianceRatio * 100}%. Please contact ${user.profile.first_name} ${user.profile.last_name}.`;
+        const statusText = `${unit} is not performing well, with a compliance ratio of ${complianceRatio * 100}%. Please contact ${name}.`;
 
-        const actions            = ActionExtractor.extract(report.progress);
+        const actions = ActionExtractor.extract(report.progress);
 
         const stringifiedActions = actions.map(function(value) {
             const name = value.name;
-            const transformed = value.actions.reduce(function(previous, current) { return `${previous}${current}\n` }, '');
+            const transformed = value.actions.reduce((previous, current, index) => `${index+1}. ${previous}${current}\n`, '');
 
-            return `Chapter: ${name}\n${stringifiedActions}`;
+            return `Chapter: ${name}\n${transformed}`;
         });
 
-        const actionsText = actions.reduce((previous, current) => `${previous}\n${current}`, '');
+        const actionsText = stringifiedActions.reduce((previous, current) => `${previous}\n${current}`, '');
 
-        console.log(actions);
-        console.log(stringifiedActions);
-        console.log(actionsText);
-
-        return statusText;
+        return `${statusText}\n\nActions to be done:\n${actionsText}`;
     }
 
     // TODO: If less, contact person responsible and prioritize actions to be done
